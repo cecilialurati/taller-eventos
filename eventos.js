9273f0a038df7c840569ba89c592b7472393aecd
CeciliaLurati <ceci.lurati@hotmail.com>
2023-08-13 22:06:55 -0300

    funcionalidad div

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const div = document.getElementById('saludarDiv');
+    div.addEventListener('click', function() {
+        alert('Hola! Soy el div');
+    });
+});
